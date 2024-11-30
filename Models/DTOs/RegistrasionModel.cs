ffdf55d6b07ec5594df5ffef68227b4a820a33fe
dangvancong2301@gmail.com <dangvancong2301@gmail.com>
2024-11-30 10:17:42 +0700

    add validate registrasioin

--- Models/DTOs/RegistrasionModel.cs
+++ Models/DTOs/RegistrasionModel.cs
@@ -4,9 +4,10 @@ public class RegistrastionModel {
     [Required(ErrorMessage = "Tên đăng nhập không được trống!")]
     public string sUserName { get; set; }
     [Required(ErrorMessage = "Email không được trống!")]
+    [RegularExpression(@"^[^@\s]+@[^@\s]+\.(com|net|org|gov)$", ErrorMessage = "Email phải chứa @.com/@.net/@.org")]
     public string sEmail { get; set; }
     [Required(ErrorMessage = "Mật khẩu không được trống!")]
-    //[RegularExpression(@"^\d.*$", ErrorMessage = "Ký tự đầu tiên phải là số")]
+    [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "Mật khẩu phải lớn hơn 8 ký tự, có ít nhất 1 chữ cái thường, hoa, ký tự đặc biệt ")]
     public string sPassword { get; set; }
     [Required(ErrorMessage = "Nhập lại mật khẩu")]
     [Compare("sPassword", ErrorMessage = "Hai mật khẩu phải giống nhau!")]
